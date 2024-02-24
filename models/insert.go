45281b3160d54306509ff592d20c829580110d6c
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-24 17:50:40 -0300

    feat: adding a todo entitie and insert func

---
+++ models/insert.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/felipeversiane/note-rest-api/db"
+)
+
+func Insert(todo Todo) (id int64, err error) {
+	conn, err := db.OppenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	sql := `INSERT INTO todos (title, description, done) VALUES ($1, $2, $3) RETURNING id`
+	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
+	return
+}
