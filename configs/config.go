3c2f24fd245985b7062abcf9f85a173f4a44b2ce
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-24 17:43:47 -0300

    feat: adding db connection

--- configs/config.go
+++ configs/config.go
@@ -1,4 +1,5 @@
 package configs
+
 import "github.com/spf13/viper"
 
 var cfg *config
@@ -21,15 +22,15 @@ type DBConfig struct {
 }
 
 func init() {
-	viper.SetDefault('api.port','9000')
-	viper.SetDefault('database.host','localhost')
-	viper.SetDefault('database.port','5432')
+	viper.SetDefault("api.port", "9000")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", "5432")
 }
 
 func Load() error {
-	viper.SetConfigName('config')
-	viper.SetConfigType('toml')
-	viper.AddConfigPath('.')
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
@@ -38,14 +39,14 @@ func Load() error {
 	}
 	cfg = new(config)
 	cfg.API = APIConfig{
-		Port: viper.GetString('api.port'),
+		Port: viper.GetString("api.port"),
 	}
-	cfg.DB = DBConfig {
-		Host: viper.GetString('database.host'),
-		Port: viper.GetString('database.port'),
-		User: viper.GetString('database.user'),
-		Pass: viper.GetString('database.pas'),
-		Database: viper.GetString('database.name'),
+	cfg.DB = DBConfig{
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetString("database.port"),
+		User:     viper.GetString("database.user"),
+		Pass:     viper.GetString("database.pas"),
+		Database: viper.GetString("database.name"),
 	}
 	return nil
 }
